32093c947e38674918d23bbd9df29053d06efe3f
aminabdo43@gmail.com <@CAshu011223234151>
2020-06-15 05:25:57 +0200

    test

--- app/src/androidTest/java/com/shoohna/happytimes/TitleScreenTest.kt
+++ app/src/androidTest/java/com/shoohna/happytimes/TitleScreenTest.kt
@@ -2,44 +2,56 @@ package com.shoohna.happytimes
 
 import androidx.lifecycle.ViewModelStore
 import androidx.navigation.testing.TestNavHostController
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
-import com.shoohna.happytimes.ui.home.MainActivity
-import com.shoohna.happytimes.ui.home.ui.favorite.FavoriteFragment
-import com.shoohna.happytimes.ui.home.ui.productDetails.ProductDetailsFragment
+import com.shoohna.happytimes.upsTask.MostPopularActivity
+import com.shoohna.happytimes.upsTask.ui.titles.TitelsFragment
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-class FavouriteScreenTest {
+class TitleScreenTest {
 
-    private lateinit var activity : MainActivity
+
+    private lateinit var activity : MostPopularActivity
     private lateinit var navController : TestNavHostController
 
     @Rule
     @JvmField
-    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+    var mActivityTestRule = ActivityTestRule(MostPopularActivity::class.java)
+
 
     @Before
     fun setUp() {
         navController = TestNavHostController(mActivityTestRule.activity!!)
         navController.setViewModelStore(ViewModelStore())
 
-        navController.setGraph(R.navigation.nav_home)
-        navController.navigate(R.id.favoriteFragment)
+        navController.setGraph(R.navigation.nav_ups)
+        navController.navigate(R.id.titles)
 
         activity = mActivityTestRule.activity
         mActivityTestRule.activity
             .supportFragmentManager
             .beginTransaction()
-            .replace(R.id.nav_host_fragment, FavoriteFragment())
+            .replace(R.id.nav_host_fragment, TitelsFragment())
             .addToBackStack(null)
             .commitAllowingStateLoss()
+
+    }
+
+    @Test
+    fun titleClick()
+    {
+        Espresso.onView(withId(R.id.tv_title)).perform(ViewActions.click())
     }
     @Test
-    fun loadData(){
-
+    fun descClick()
+    {
+        Espresso.onView(withId(R.id.tv_desc)).perform(ViewActions.click())
     }
 }
--- app/src/test/java/com/shoohna/happytimes/welcome/RegisterFunctionTest.kt
+++ app/src/test/java/com/shoohna/happytimes/welcome/RegisterFunctionTest.kt
@@ -2,22 +2,92 @@ package com.shoohna.happytimes.welcome
 
 import androidx.lifecycle.MutableLiveData
 import com.google.android.material.textfield.TextInputLayout
-import com.shoohna.e_commercehappytimes.ui.welcome.ui.login.LoginFragment
 import com.shoohna.happytimes.R
-import com.shoohna.happytimes.ui.welcome.WelcomeActivity
-import com.shoohna.happytimes.ui.welcome.ui.register.RegisterViewModel
-import kotlinx.android.synthetic.main.bottom_sheet_layout.*
+import com.shoohna.happytimes.upsTask.ui.titles.TitelsViewModel
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+import android.content.Context
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import com.shoohna.happytimes.networking.interfaces.ups
+import com.shoohna.happytimes.pojo.ups.Result
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+
 import org.koin.android.ext.android.inject
+
+import org.koin.android.ext.android.inject
+
 //import org.koin.java.KoinJavaComponent.inject
 
-class RegisterFunctionTest {
-//
-//    var vm: RegisterViewModel by inject()   // 1
-//
+class RegisterFunctionTest (private val serviceGeneral: ups) : ViewModel(){
+
+    private var mostPopularList: MutableLiveData<List<Result>>? = null
+
+    var vm: TitelsViewModel   by inject()
+
+    private fun loadData(context: Context) {
+
+
+        try {
+//            val service = ApiClient.makeRetrofitServiceHome()
+            CoroutineScope(Dispatchers.IO).async {
+
+                runCatching {
+                    serviceGeneral.get_UPS()
+                }.onSuccess {
+                    withContext(Dispatchers.Main) {
+                        try {
+                            Log.e("response","response 6666 ==> "+ it.body()!!.copyright)
+
+                            if (it.isSuccessful ) {
+                                Log.e("response","response 232 ==> "+ it.body()!!.copyright)
+//                            Toast.makeText(context, response.body()!!.message, Toast.LENGTH_SHORT)
+//                                .show()
+//                        baseFragment.showDialog(context,response.body()!!.message)
+                                mostPopularList!!.postValue(it.body()!!.results)
+                                //Toast.makeText(context , "done" , Toast.LENGTH_LONG).show()
+                            } else {
+                                Log.i("loadData1", it.message().toString())
+                                Toast.makeText(context, it.message(), Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        } catch (e: Exception) {
+                            Log.i("loadData2", e.message.toString())
+                            Toast.makeText(
+                                context,
+                                "Exception ${e.message.toString()}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                        }
+
+                    }
+                }.onFailure {
+                    Toast.makeText(context,it.message.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }catch (e:Exception)
+        {
+            Toast.makeText(context,e.message.toString(), Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
+    internal fun getTitles(context: Context): MutableLiveData<List<Result>> {
+        if (mostPopularList == null) {
+            mostPopularList = MutableLiveData()
+            loadData(context)
+        }
+        return mostPopularList as MutableLiveData<List<Result>>
+    }
 //    var activity : WelcomeActivity = WelcomeActivity()
 //
 //
@@ -44,3 +114,7 @@ class RegisterFunctionTest {
 //
 //    }
 }
+
+
+
+
